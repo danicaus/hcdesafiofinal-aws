0b214b174cbe9638dfd7b75d4e01252854e1395e
Daniela Caus <danicaus.br@gmail.com>
2021-08-30 14:23:30 -0300

    Add validation and confirmation to leads-form component

--- react/LeadsForm.jsx
+++ react/LeadsForm.jsx
@@ -3,50 +3,88 @@ import { useCssHandles } from 'vtex.css-handles'
 import { useState } from 'react';
 import '../styles/css/leads-form.css'
 
-const CSS_HANDLES = ['LeadsFormContainer', 'LeadsFormInputContainer', 'LeadsFormLabel', 'LeadsFormInput', 'LeadsFormButton']
+const CSS_HANDLES = ['LeadsFormContainer', 'LeadsFormInputContainer', 'LeadsFormLabel', 'LeadsFormInput', 'LeadsFormButton', 'LeadsFormValidation', 'FormConfirmationTitle']
 
 
 const LeadsForm = () => {
   const handles = useCssHandles(CSS_HANDLES)
+
+  const [subscribed, setSubscribed] = useState(false)
+  
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [telephone, setTelephone] = useState('')
-  
+
   async function handleForm(e) {
     e.preventDefault()
 
-    const formData  = {
+    const formData = {
       body: {
         name: name,
         email: email,
         telephone: telephone
       }
     }
-    //código para enviar para a API aqui
+    //Inserir código para enviar dados para a API aqui
 
     console.log(formData);
+
+
+    //Colocar a mudança abaixo somente se o form foi enviado com sucesso
+    setSubscribed(true)
+
+    //Caso o form não tenha sido enviado com sucesso, colocar o alert
+   // alert("Erro ao se conectar ao servidor")
   }
 
-  return (
-    <div>
-      <form className={`${handles.LeadsFormContainer}`} onSubmit={handleForm}>
+  function handleValidation(input) {
+    const validation = input.validity.valid
+
+    if (!validation) {
+      input.parentElement.classList.add(`${handles.LeadsFormInputContainer}--invalid`)
+    } else {
+      input.parentElement.classList.remove(`${handles.LeadsFormInputContainer}--invalid`)
+    }
+  }
+
+  const renderForm = ()=> {
+    return (
+      <form onSubmit={handleForm}>
         <div className={`${handles.LeadsFormInputContainer}`}>
           <label className={`${handles.LeadsFormLabel}`} htmlFor="personName">Nome</label>
-          <input className={`${handles.LeadsFormInput}`} onChange ={e => setName(e.target.value)} type="text" name="personName" required id="personName" placeholder="Digite seu nome" />
+          <input className={`${handles.LeadsFormInput}`} onBlur={(e) => handleValidation(e.target)} onChange={e => setName(e.target.value)} type="text" minLength="3" name="personName" required id="personName"/>
+          <span className={`${handles.LeadsFormValidation}`}> É necessário seu nome completo</span>
         </div>
 
         <div className={`${handles.LeadsFormInputContainer}`}>
           <label className={`${handles.LeadsFormLabel}`} htmlFor="personEmail">E-mail</label>
-          <input className={`${handles.LeadsFormInput}`} onChange={e => setEmail(e.target.value)} type="email" pattern=".+@.+\.com" name="personEmail" id="personEmail" required placeholder="Digite seu email" />
+          <input className={`${handles.LeadsFormInput}`} onBlur={(e) => handleValidation(e.target)} onChange={e => setEmail(e.target.value)} type="email" pattern=".+@.+\.com" name="personEmail" id="personEmail" required/>
+          <span className={`${handles.LeadsFormValidation}`}> Um endereço de e-mail válido é necessário</span>
         </div>
 
         <div className={`${handles.LeadsFormInputContainer}`}>
           <label className={`${handles.LeadsFormLabel}`} htmlFor="personTel">Telefone</label>
-          <input className={`${handles.LeadsFormInput}`} onChange={e => setTelephone(e.target.value)} type="text" name="personTel" id="personTel" required placeholder="Digite seu telefone" />
+          <input className={`${handles.LeadsFormInput}`} onBlur={(e) => handleValidation(e.target)} onChange={e => setTelephone(e.target.value)} type="tel" minLength="9" name="personTel" id="personTel" required/>
+          <span className={`${handles.LeadsFormValidation}`}> É necessário um telefone válido</span>
         </div>
 
         <button className={`${handles.LeadsFormButton}`} type="submit">Eu quero!</button>
       </form>
+    )
+  }
+
+  const renderConfirmation = ()=> {
+    return (
+      <>
+        <p className={`${handles.FormConfirmationTitle}`}>Seu e-mail foi cadastrado com sucesso!</p>
+        <p className={`${handles.LeadsFormLabel}`}>A partir de agora você receberá nossas novidades e ofertas exclusivas.</p>
+      </>
+    )
+  }
+
+  return (
+    <div className={`${handles.LeadsFormContainer}`}>
+      {subscribed ? renderConfirmation() : renderForm()}
     </div>
   )
 }
